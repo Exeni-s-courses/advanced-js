33a66c866f43de0ac5d36dda241fdd3dae803a96
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-02-08 22:44:21 -0400

    consume the promise

---
+++ src/concepts/03-promises.js
@@ -0,0 +1,37 @@
+import { heroes } from "../data/heroes"
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const promiseComponent = (element) => {
+    
+    
+    const renderHero = (hero) => {
+        element.innerHTML = hero.name;
+    }
+    
+    const renderError = (error) => {
+        element.innerHTML = `
+            <h3> ${error} </h3>
+        `;
+    }
+    const id1 = '5d86371f9f80b591f499df32';
+    findHero(id1).then(renderHero).catch(renderError)
+}
+
+/**
+ * 
+ * @param {String} id 
+ * @returns { Promise }
+ */
+const findHero = (id) => {
+    return new Promise((resolve, reject) => {
+        const hero = heroes.find(hero => hero.id === id);
+        if (!hero) {
+            reject(`Hero with id ${id} not found`);
+            return;
+        };
+        resolve(hero);
+    });
+}
