4ce8ec14b1291a90216f0c3a0b6cf594a25faac1
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-02-09 22:08:21 -0400

    promise all

--- src/concepts/03-promises.js
+++ src/concepts/03-promises.js
@@ -10,6 +10,10 @@ export const promiseComponent = (element) => {
     const renderHero = (hero) => {
         element.innerHTML = hero.name;
     }
+  
+    const renderTwoHero = (hero1, hero2) => {
+        element.innerHTML = `${hero1.name} - ${hero2.name}`;
+    }
     
     const renderError = (error) => {
         element.innerHTML = `
@@ -17,7 +21,32 @@ export const promiseComponent = (element) => {
         `;
     }
     const id1 = '5d86371f9f80b591f499df32';
-    findHero(id1).then(renderHero).catch(renderError)
+    const id2 = '5d86371fd55e2e2a30fe1ccb1';
+
+
+    // forma 1
+    // findHero(id1).then((hero1) => {
+        
+    //     findHero(id2).then((hero2) => {
+    //         renderTwoHero(hero1, hero2);
+    //     }).catch(renderError)
+
+    // }).catch(renderError)
+
+    // forma 2
+    // let hero1;
+    // findHero(id1).then(hero => {
+    //     hero1 = hero;
+    //     return findHero(id2);
+    // }).then(hero2 => {
+    //     renderTwoHero(hero1, hero2);
+    // }).catch(renderError)
+
+    // forma 3
+    Promise.all([
+        findHero(id1),
+        findHero(id2)
+    ]).then(([hero1, hero2]) => renderTwoHero(hero1, hero2)).catch(renderError)
 }
 
 /**
