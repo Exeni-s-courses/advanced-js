4d8f05c96636d98284cc76ddbbf4f6562d2058ce
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-02-11 14:14:08 -0400

    error handle async await functions

--- src/concepts/06-async-await.js
+++ src/concepts/06-async-await.js
@@ -8,10 +8,16 @@ export const asyncAwaitComponent = async (element) => {
     const id1 = '5d86371fd55e2e2a30fe1cc3';
     const id2 = '5d86371fd55e2e2a30fe1cc4';
 
-    const {name: name1} = await findHero(id1);
-    const {name: name2} = await findHero(id2);
 
-    element.innerHTML = `${name1} / ${name2}`;
+    try {
+        const { name: name1 } = await findHero(id1);
+        const { name: name2 } = await findHero(id2);
+
+        element.innerHTML = `${name1} / ${name2}`;
+    } catch (error) {
+        element.innerHTML = error;
+    }
+
 
 }
 
