fd0d6c2e968e883c49f53dda471036d65a98b232
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-02-11 14:46:13 -0400

    example use generator function

--- src/concepts/09-generators.js
+++ src/concepts/09-generators.js
@@ -5,12 +5,36 @@
  */
 export const generatorsComponent = (element) => {
 
-    const myGenerator = myFisrtGeneratorFunction();
+    // const myGenerator = myFisrtGeneratorFunction();
 
-    console.log(myGenerator.next())
+    // console.log(myGenerator.next())
     
+    const genId = idGenerator();
+
+    const button = document.createElement('button');
+    button.innerText = 'Click me';
+    element.append(button);
+
+
+    const renderButton = () => {
+        const { value } = genId.next();
+        button.innerText = `Click ${ value }`
+    }
+
+    console.log(genId.next());
+
+
+    button.addEventListener('click', renderButton);
+
 }
 
+
+function* idGenerator() {
+    let currentId = 0;
+    while(true){
+        yield ++currentId;
+    }
+}
 
 function* myFisrtGeneratorFunction() {
 
