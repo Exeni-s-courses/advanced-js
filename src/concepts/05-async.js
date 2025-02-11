aae709714c2e538196c90900e82663b0a1a3b14f
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-02-11 14:04:20 -0400

    error handle for async functions

--- src/concepts/05-async.js
+++ src/concepts/05-async.js
@@ -6,7 +6,13 @@ import { heroes } from '../data/heroes'
 export const asyncComponent = (element) => {
 
     const id = '5d86371fd55e2e2a30fe1cc3';
-    findHero(id).then(element.innerHTML).catch(element.innerHTML)
+
+    console.log('Inicio del componente');
+    
+    // findHero(id).then(console.log).catch(element.innerHTML)
+    findHero(id).then(name => element.innerHTML = name).catch(error => element.innerHTML = error)
+    
+    console.log('Fin del componente');
 }
 
 
@@ -17,5 +23,6 @@ export const asyncComponent = (element) => {
  */
 const findHero = async (id) => {
     const hero = heroes.find(hero => hero.id === id);
+    if(!hero) throw new Error(`Hero with id ${id} not found`);
     return hero.name;
 }
